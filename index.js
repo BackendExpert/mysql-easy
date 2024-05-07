e23dacb1b44b845b5fb117f19b6e7cc60ab55402
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 18:51:18 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const mysql = require('mysql2');
 
-function ConnectDatabase(dbHost, dbUser, dbPass, dbName) {
+async function ConnectDatabase(dbHost, dbUser, dbPass, dbName) {
     const connection = mysql.createConnection({
         host: dbHost,
         user: dbUser,
         password: dbPass,
         database: dbName
     })
+
+    // Connect to MySQL
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL');
+            return;
+        }
+        console.log('Connected to MySQL as id ');
+    });
+
+    return connection;
 }
 
-module.exports = {
-    ConnectDatabase
-};
+module.exports = ConnectDatabase;
