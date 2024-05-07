30bfb5b59151fafc8d163667315515dcdfd9bf8e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 18:09:03 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const mysql = require('mysql');
+
+function ConnectDatabase(dbHost, dbUser, dbPass, dbName) {
+    const connection = mysql.createConnection({
+        host: dbHost,
+        user: dbUser,
+        password: dbPass,
+        database: dbName
+    })
+}
