1b9d88a2601122d05b6406b9ac9dcdac8c0fa251
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 20:04:27 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-function ConnectDatabase(host, user, password, database) {
+function ConnectToDatabase(host, user, password, database) {
     return mysql.createConnection({
         host: host,
         user: user,
@@ -8,3 +8,17 @@ function ConnectDatabase(host, user, password, database) {
         database: database
     });
 }
+
+function selectDataByColumns(connection, tableName, columnsData, callback) {
+    const query = `SELECT * FROM ${tableName} WHERE ?`;
+
+    connection.query(query, columnsData, (error, results, fields) => {
+        if (error) throw error;
+        callback(results);
+    });
+}
+
+module.exports = {
+    ConnectToDatabase,
+    selectDataByColumns,
+};
