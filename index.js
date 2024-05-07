2f9bf01bbf967e458446318864fc5b3b7a9cfe76
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-07 19:17:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
+// mysqlConnection.js
 const mysql = require('mysql2');
 
-async function ConnectDatabase(dbHost, dbUser, dbPass, dbName) {
+// Function to establish MySQL connection
+function connectToMySQL(host, user, password, database) {
+    // MySQL connection configuration
     const connection = mysql.createConnection({
-        host: dbHost,
-        user: dbUser,
-        password: dbPass,
-        database: dbName
-    })
-
-    // Connect to MySQL
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to MySQL');
-            return;
-        }
-        console.log('Connected to MySQL as id ');
+        host: host,
+        user: user,
+        password: password,
+        database: database
     });
 
+    // Return the connection object
     return connection;
 }
 
-module.exports = ConnectDatabase;
+module.exports = {
+    connectToMySQL
+};
