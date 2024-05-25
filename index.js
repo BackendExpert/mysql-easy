85ace43139e7a1b52896f1e934e8d50d262429cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-25 18:32:13 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- index.js
+++ index.js
@@ -176,7 +176,12 @@ function SearchData(connection, tableName, searchColumns, callback) {
 
 
 function CountData(connection, tableName, countas, conditions, callback){
-
+    let query = `SELECT COUNT(*) AS count FROM ${tableName}`
+    if(conditions && conditions.length > 0) {
+        const whereClause = conditions.join(' AND ')
+        query += ` WHERE ${whereClause}`;
+    }
+    
 }
 
 
